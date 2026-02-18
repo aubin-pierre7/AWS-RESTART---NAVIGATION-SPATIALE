7a61204ad35ec859e8d003cfd6e2d03fa444f2ed
aubin-pierre7 <aubinp58@gmail.com>
2026-02-18 15:13:44 +0100

    Creation du fichier pour la tache 2

---
+++ tache2.py
@@ -0,0 +1,29 @@
+import os
+
+dossier = "mission_data"
+
+# Verification de l'existence
+if not os.path.exists(dossier):
+    print(" Le dossier mission_data n'existe pas")
+    exit()
+
+print("mission_data/")
+
+# Lister fichiers
+for element in os.listdir(dossier):
+
+    chemin = os.path.join(dossier, element)
+
+    if os.path.isfile(chemin):
+        taille = os.path.getsize(chemin) / 1024
+        print(f"    {element} ({taille:.1f} Ko)")
+
+# Creer des sous dossiers
+rapports = os.path.join(dossier, "rapports")
+archives = os.path.join(dossier, "archives")
+
+os.makedirs(rapports, exist_ok=True)
+os.makedirs(archives, exist_ok=True)
+
+print("    rapports/")
+print("    archives/")
